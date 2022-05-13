207e7fc32f60ee2b8c8b75d3bb8b6ef10d96967c
Ahmet Kutay Karacair <39398963+ahmetkutay@users.noreply.github.com>
2022-05-13 23:56:16 +0300

    updated filter_start, findmax and valueCounter files

--- 6 Other Algorithms/findmax_start.py
+++ 6 Other Algorithms/findmax_start.py
@@ -4,15 +4,23 @@
 # declare a list of values to operate on
 items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
+
 def find_max(items):
     # TODO: breaking condition: last item in list? return it
-
+    if len(items) == 1:
+        return items[0]
 
     # TODO: otherwise get the first item and call function
     # again to operate on the rest of the list
-
-
+    option1 = items[0]
+    print(option1)
+    option2 = find_max(items[1:])
+    print(option2)
     # TODO: perform the comparison when we're down to just two
+    if option1 > option2:
+        return option1
+    else:
+        return option2
 
 
 # test the function
